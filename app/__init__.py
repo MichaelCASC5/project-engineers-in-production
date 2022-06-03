efe373dbdde466a1a0e946fcf763e8bc32c557d4
PAOK-2001 <2001.paok@gmail.com>
2022-06-02 19:45:04 -0500

    Refactored page to use routing and Jinja templates

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-
+images ={
+    "logo": "/static/img/logo.svg",
+    "profilePic" : "/static/img/michael.jpg"
+}
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template(
+        'landingPage.html', 
+        title     = "MLH Fellow",
+        images    = images,
+        url       = os.getenv("URL")
+    )
+
+@app.route('/work')
+def work():
+    return render_template(
+        'Work-Education.html',
+        images    = images,
+
+    )
