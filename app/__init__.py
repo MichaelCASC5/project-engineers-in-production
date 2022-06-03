f431caf54835855cfe341f685cf68b93692ebbfb
PAOK-2001 <2001.paok@gmail.com>
2022-06-03 10:38:25 -0500

    Added Jinja templating for work and education section

--- app/__init__.py
+++ app/__init__.py
@@ -20,8 +20,18 @@ def index():
 
 @app.route('/work')
 def work():
+    jobs = [
+        ["/static/img/Work_Education/Job1.jpeg","Major League Hacking","Production Engineer Fellow", "This is a description of what I did in this job", "June 2022 - August 2022"],
+    ]
+
+    education = [
+        ["/static/img/Work_Education/Job1.jpeg","Major League Hacking","Pre Fellow", "This is a description of what I did in this job", "April 2022"],
+        ["static/img/Work_Education/Edu1.jpeg", "Tec de Monterrey","BS. in Robotic and Digital Systems"],
+    ]
     return render_template(
         'Work-Education.html',
         images    = images,
+        jobs = jobs,
+        education = education
 
     )
