944b19df903cf6235e6b69494da7f11e550ab01d
Michael <michaelcalle14@gmail.com>
2022-06-23 18:55:18 -0400

    largeupdate

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,38 @@
+from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict
 import os
 from flask import Flask, render_template, request, json
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
-images ={
+
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+
+print("firstprint",mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+images = {
     "logo": "/static/img/logo.svg",
-    "profilePic" : "/static/img/logo.jpg"
+    "profilePic" : "/static/img/logo.jpg",
+    "desk" : "/static/img/desk_background.jpg"
 }
 
 @app.route('/')
@@ -38,9 +64,45 @@ def hobbies():
     data = json.load(open("./app/static/hobbies.json"))
     footer = json.load(open("./app/static/footer.json"))
     return render_template(
-        'Hobbies.html',
+        'hobbies.html',
         images = images,
         data = data,
         hobbies = data["Hobbies"],
+        conclusion = data["Conclusion"],
         info = footer["FooterInformation"],
     )
+
+@app.route('/timeline')
+def timeline():
+    data = json.load(open("./app/static/hobbies.json"))
+    footer = json.load(open("./app/static/footer.json"))
+    return render_template(
+        'timeline.html',
+        images = images,
+        data = data,
+        hobbies = data["Hobbies"],
+        conclusion = data["Conclusion"],
+        info = footer["FooterInformation"],
+    )
+
+# @app.route('/timeline')
+# def timeline():
+#     return render_template('timeline.html',title="Timeline")
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
