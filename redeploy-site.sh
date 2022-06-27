be27ed36a7a493d91644173392db4693f5ad52e7
MichaelCASC5 <41081713+MichaelCASC5@users.noreply.github.com>
2022-06-27 09:24:26 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
-tmux kill-server
+#tmux kill-server
 cd project-engineers-in-production
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+#pip install -r requirements.txt
+systemctl daemon-reload
+systemctl restart myportfolio
 tmux new -d "flask run --host=0.0.0.0"
